2fdaac9e6b838db5f480bb3b77052806e7780539
MD Arora <mdarora0001@gmail.com>
2021-06-01 18:44:32 +0530

    Login functionality added

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,18 @@
 import React, {useState} from 'react';
-import {Link} from "react-router-dom";
-import spinner from "../images/Spinner-1s-200px-white.svg";
+import {Link, useHistory} from "react-router-dom";
+import cookies from 'js-cookie';
+import spinner from "../images/Spinner-1s-200px-blue.svg";
 
 const Login = () => {
 
+    const history = useHistory();
+    if (cookies.get('jwtoken')){
+        history.push('/');
+    }
+
     const [loginEmail , setloginEmail] = useState("");
     const [loginpassword , setloginpassword] = useState("");
+    const [resText, setResText] = useState('')
 
     const showPass = (e) =>{
         if(e.target.checked){
@@ -15,9 +22,39 @@ const Login = () => {
         }
     }
 
-    const userLogin = (e) =>{
+    const userLogin = async (e) =>{
         e.preventDefault();
-    }
+        const spinner = document.getElementById('spinner');
+
+        try {
+            setResText('');
+            spinner.hidden = false;
+            const res = await fetch('/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                   email:  loginEmail,
+                   password: loginpassword
+                })
+            });
+
+            const result = await res.json();
+            if (result.message){
+                spinner.hidden = true;
+                return history.push("/");
+            } else if (result.error){
+                spinner.hidden = true;
+                setResText(result.error);
+            }
+
+        } catch (error) {
+            console.log('catch : ',error);
+            spinner.hidden = true;
+            setResText('Something went wrong!');
+        }
+    };
 
     return (
     <>
@@ -36,15 +73,15 @@ const Login = () => {
                             </div>
                             <div className="show-forgot-password">
                                 <div className="show-pass">
-                                    <input type="checkbox" name="show-pass" id="show-pass" onChange={(showPass)} />
-                                    <span>Show Password</span>
+                                    <input type="checkbox" name="show-pass" id="show-pass" onChange={showPass} />
+                                    <label htmlFor='show-pass'>Show Password</label>
                                 </div>
                                 <div className="forgot-pass">
                                     <Link to="/reset-password">Forgot Password ?</Link>
                                 </div>
                             </div>
-                            <div id="response" className="res text-danger">
-                                <span id="res-text"></span>
+                            <div className="res">
+                                <span id="res-text" className='text-danger'>{resText}</span>
                                 <img id="spinner" src={spinner} alt="Spinner svg" width="30" hidden/>
                             </div>
                             <div className="form-btn-container">
